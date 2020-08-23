19faad58bdbd849283059b41411208669eba519d
corbin009 <user@users-MacBook-Pro.local>
2020-08-23 17:19:07 +0600

    WIP: transaction validation

---
+++ api/validator/transactionValidate.js
@@ -0,0 +1,20 @@
+const validate = (transaction) => {
+    let error = {}
+    if(!transaction.amount){
+        error.amount = "Please set a minimum amount"
+    }
+
+    if(!transaction.type){
+        error.type = "Please provide a transaction type"
+    }
+
+    if(!transaction.note){
+        error.note = "Please provide a transaction note"
+    }
+
+    return {
+        error,
+        isValid: Object.keys(error).length === 0 
+    }
+
+}
