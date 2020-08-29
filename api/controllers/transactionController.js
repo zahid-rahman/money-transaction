07559046d3361574290ccba57dbb9ede672a097b
zahid-rahman <rahmanzahid94@gmail.com>
2020-08-29 22:16:05 +0600

    WIP:adding transaction controller

---
+++ api/controllers/transactionController.js
@@ -0,0 +1,96 @@
+let Transaction = require('../models/Transaction')
+let User = require('../models/User')
+
+// transaction list 
+let findTransactionList = (req,res) => {
+    Transaction.find()
+    .then(transactions => {
+        if(transactions.length === 0){
+            res.json({
+                message : "No transaction found"
+            })
+        }else{
+            res.json(transactions)
+        }
+    })
+    .catch(error => console.log(error))
+}
+
+// create valid transaction 
+let createTransaction = (req,res) => {
+    let {amount,type,note} = req.body
+    let userId = req.user._id
+
+    console.log(req.user)
+
+    let transaction = new Transaction({
+        amount, type, note, author : userId
+    })
+
+    transaction.save()
+        .then(trans => {
+            let updateUser = {...req.user._doc}
+            if(type === "income"){
+                updateUser.balance = updateUser.balance + amount
+                updateUser.income = updateUser.income + amount
+            }
+            else if(type === 'expense'){
+                updateUser.balance = updateUser.balance - amount
+                updateUser.expense = updateUser.expense + amount
+            }
+            console.log(updateUser)
+            updateUser.transactions.unshift(trans._id)
+
+            User.findByIdAndUpdate(updateUser._id,{$set:updateUser},{new:true})
+                .then(() => {
+                    res.json({
+                        message : 'Transaction created successfully'
+                    })
+                })
+
+        })
+        .catch(error => console.log(error))
+}
+
+// single transaction 
+let findSingleTransaction = (req,res) => {
+    let { transactionId } = req.params
+    Transaction.findById(transactionId)
+    .then(transaction => {
+        res.json(transaction)
+    })
+    .catch(error => console.log(error))
+}
+
+// delete transaction 
+let deleteSingleTransaction = (req,res) => {
+    let { transactionId } = req.params
+    Transaction.findByIdAndDelete(transactionId)
+    .then(() => {
+        res.json({
+            message : "Transaction deleted successfully"
+        })
+    })
+    .catch(error => console.log(error))
+}
+
+// update a transaction
+let updateSingleTransaction = (req,res) => {
+    let { transactionId } = req.params
+    Transaction.findByIdAndUpdate(transactionId,{$set:req.body})
+    .then(() => {
+        res.json({
+            message : "transaction updated successfully"
+        })
+    })
+    .catch(error => console.log(error))
+}
+
+module.exports = {
+    findTransactionList,
+    createTransaction,
+    findSingleTransaction,
+    updateSingleTransaction,
+    deleteSingleTransaction,
+
+}
--- api/router/transaction.js
+++ api/router/transaction.js
@@ -3,90 +3,21 @@ let Transaction = require('../models/Transaction')
 let User = require('../models/User')
 
 let authenticate = require('../middleware/authenticate');
+const transactionController = require('../controllers/transactionController');
 
 // list of all transaction
-router.get('/',authenticate,(req,res) => {
-    Transaction.find()
-    .then(transactions => {
-        if(transactions.length === 0){
-            res.json({
-                message : "No transaction found"
-            })
-        }else{
-            res.json(transactions)
-        }
-    })
-    .catch(error => console.log(error))
-})
+router.get('/list',transactionController.findTransactionList)
 
 // create a valid transaction
-router.post('/create', authenticate, (req,res) => {
-    let {amount,type,note} = req.body
-    let userId = req.user._id
-
-    console.log(req.user)
-
-    let transaction = new Transaction({
-        amount, type, note, author : userId
-    })
-
-    transaction.save()
-        .then(trans => {
-            let updateUser = {...req.user._doc}
-            if(type === "income"){
-                updateUser.balance = updateUser.balance + amount
-                updateUser.income = updateUser.income + amount
-            }
-            else if(type === 'expense'){
-                updateUser.balance = updateUser.balance - amount
-                updateUser.expense = updateUser.expense + amount
-            }
-            console.log(updateUser)
-            updateUser.transactions.unshift(trans._id)
-
-            User.findByIdAndUpdate(updateUser._id,{$set:updateUser},{new:true})
-                .then(() => {
-                    res.json({
-                        message : 'Transaction created successfully'
-                    })
-                })
-
-        })
-        .catch(error => console.log(error))
-})
+router.post('/create', authenticate,transactionController.createTransaction )
 
 //single transaction 
-router.get('/:transactionId',(req,res) => {
-    let { transactionId } = req.params
-    Transaction.findById(transactionId)
-    .then(transaction => {
-        res.json(transaction)
-    })
-    .catch(error => console.log(error))
-})
+router.get('/:transactionId',transactionController.findSingleTransaction)
 
 // delete transaction
-router.delete('/:transactionId',(req,res) => {
-    let { transactionId } = req.params
-    Transaction.findByIdAndDelete(transactionId)
-    .then(() => {
-        res.json({
-            message : "Transaction deleted successfully"
-        })
-    })
-    .catch(error => console.log(error))
-})
+router.delete('/:transactionId',transactionController.deleteSingleTransaction)
 
 // update transaction
-router.patch('/:transactionId',(req,res) => {
-    let { transactionId } = req.params
-    Transaction.findByIdAndUpdate(transactionId,{$set:req.body})
-    .then(() => {
-        res.json({
-            message : "transaction updated successfully"
-        })
-    })
-    .catch(error => console.log(error))
-})
+router.patch('/:transactionId',transactionController.updateSingleTransaction)
 
 module.exports = router;
